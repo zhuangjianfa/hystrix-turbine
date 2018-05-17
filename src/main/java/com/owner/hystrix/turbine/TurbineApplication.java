c9b2f0d2c27a9b91661fa0d47986af04d7acff8b
zhuangjianfa <zhuangjianfa@192.168.1.153>
2018-05-17 18:24:24 +0800

    hystrix turbine监控

---
+++ src/main/java/com/owner/hystrix/turbine/TurbineApplication.java
@@ -0,0 +1,23 @@
+package com.owner.hystrix.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 
+ * description:熔断器监控
+ * Author	Date	Changes
+ * zhuangjianfa  2018年5月17日 Created
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+
+}
